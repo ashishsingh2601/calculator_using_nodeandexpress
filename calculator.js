2cdb4430482b71237d241580b94356ef4e3c830e
Ashish <as7220004@gmail.com>
2020-02-04 13:49:48 +0530

    styledHomePage

--- calculator.js
+++ calculator.js
@@ -6,8 +6,15 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
+//app.use(express.static(__dirname, '/public'));
+
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
+
+});
+
+app.get("/public/css/style.css", function(req, res){
+    res.sendFile(__dirname + "/public/css/style.css");
 
 });
 
