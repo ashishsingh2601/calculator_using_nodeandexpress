50b3e275d0ba1698f1ce1b5d718a062f9d8f5c24
Ashish <as7220004@gmail.com>
2020-02-04 12:35:35 +0530

    additionOfTwoNumsDone

--- calculator.js
+++ calculator.js
@@ -1,11 +1,22 @@
 //jshint esversion:6
 
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
-    res.send("<h1>Hello World!</h1>");
+    res.sendFile(__dirname + "/index.html");
 
+});
+
+app.post("/", function(req, res){
+  var number1 =  Number(req.body.num1);  //parsing data as number
+  var number2 = Number(req.body.num2);  //by default is get parsed as text
+  
+  var result = number1+ number2;
+  res.send(`Result of Adding first and second number: ${result}`);
 });
 
 app.listen(4000, function(){
